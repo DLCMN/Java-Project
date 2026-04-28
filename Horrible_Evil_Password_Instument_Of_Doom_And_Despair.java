551b2c1a16d6bc4ab7875d048baab9f7108f84ec
Jaytat00 <emz.doolan07@gmail.com>
2026-04-28 16:37:52 +0100

    Update Horrible_Evil_Password_Instument_Of_Doom_And_Despair.java

--- Horrible_Evil_Password_Instument_Of_Doom_And_Despair.java
+++ Horrible_Evil_Password_Instument_Of_Doom_And_Despair.java
@@ -1,16 +1,18 @@
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class Horrible_Evil_Password_Instument_Of_Doom_And_Despair{
 
 	public static void main(String[] args){
 		JFrame frame = new JFrame("Happy Coding");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+			JButton button = new JButton("Click me!");
+		frame.add(button);
 		
-		JLabel label = new JLabel("Hello world!");
-		frame.add(label);
-		
-		frame.setSize(300, 300);	
+		frame.setSize(400, 400);	
 		frame.setVisible(true);
 	}
 }
