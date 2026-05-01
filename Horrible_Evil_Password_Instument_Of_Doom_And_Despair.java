ad4479ac541a7dd3935eb77c205dfe1a08b04549
Jaytat00 <emz.doolan07@gmail.com>
2026-05-01 22:38:55 +0100

    CARDLAYOUTSWITCHEROOO

--- Horrible_Evil_Password_Instument_Of_Doom_And_Despair.java
+++ Horrible_Evil_Password_Instument_Of_Doom_And_Despair.java
@@ -2,6 +2,8 @@
 import javax.swing.*;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.CardLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -13,58 +15,73 @@ import javax.sound.sampled.Clip;
 
 
 
-public class Horrible_Evil_Password_Instument_Of_Doom_And_Despair{
+public class Horrible_Evil_Password_Instument_Of_Doom_And_Despair extends JFrame {
 
-	
-	static Boolean Instchose = false;
+	CardLayout instrument;
+    JPanel mainPanel;
+    JPanel card1;
+    JPanel card2;
+
+    JButton buttonInstSkip = new JButton("Go to Card 2");
+    JButton buutun = new JButton("Go to Card 1");
+
+
+// section 1 variables
+    JButton openSecondFrame;
+    JLabel worstSound;
+    JPanel WSPanel;
+    JButton buttonInst1;
+    JButton buttonInst2;
+    JButton buttonInst3;
+    JButton buttonInst4;
+    JButton button1;
+    String guitar = "GuitarStrum.wav";
+	String harmonica = "Harmonica.wav";
+	String meow = "Meow.wav";
+	String moo = "Moo.wav";
+
+	ImageIcon cat = new ImageIcon("happyCat.png");
+	ImageIcon CowBell = new ImageIcon("CowBell.png");
+	ImageIcon Guitar = new ImageIcon("SpongebobGuitar.png");
+	ImageIcon Harmonica = new ImageIcon("harmonicaSquirrel.png");
+
+//section 2 variables
+
+    JButton maideButton;
+    JButton openFirstFrame;
+    JTextField maidenNameField;
+    JPanel maidenPanel;
+
+    static double a;
+    static double b;
+    static double c;
+    static int randomTempo;
+    static double tempo;
+    static String maidenName = "";
+    static Boolean Instchose = false;
 	static String chosenInstrument = "";
 
-	
-	public static void main(String[] args){
+    Horrible_Evil_Password_Instument_Of_Doom_And_Despair() {
 
-		String guitar = "GuitarStrum.wav";
-		String harmonica = "Harmonica.wav";
-		String meow = "Meow.wav";
-		String moo = "Moo.wav";
 
-		
-
-		ImageIcon cat = new ImageIcon("happyCat.png");
-		ImageIcon CowBell = new ImageIcon("CowBell.png");
-		ImageIcon Guitar = new ImageIcon("SpongebobGuitar.png");
-		ImageIcon Harmonica = new ImageIcon("harmonicaSquirrel.png");
+		card1 = new JPanel((new FlowLayout(FlowLayout.CENTER, 65,30)));
+		card2 = new JPanel((new FlowLayout(FlowLayout.CENTER, 65, 20)));
+		mainPanel = new JPanel();
+		instrument = new CardLayout(40, 20);
+		mainPanel.setLayout(instrument);
 
 
 
 
 
-		JFrame f = new JFrame("Hi Alan T Ryan :33");
-			f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			try {
-				UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-			} catch (Exception e) {
-        	    System.out.println("Look and Feel not set: " + e.getMessage());
-			}
 
-
-		
-
-
-
-
-	
-		
-			JPanel WSPanel = new JPanel();
-				WSPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 35, 20));
-
-
-				JLabel worstSound = new JLabel("Which is the worst sound?");
+				    worstSound = new JLabel("Which is the worst sound?");
 					worstSound.setFont(new Font("Serif", Font.BOLD, 30 ));
 					worstSound.setHorizontalAlignment(JLabel.CENTER);
-					WSPanel.add(worstSound);
+					card1.add(worstSound);
 
 
-				JButton buttonInst1 = new JButton(cat);
+				    buttonInst1 = new JButton(cat);
 
 					buttonInst1.addActionListener(new ActionListener() {
 						public void actionPerformed(ActionEvent e)            {
@@ -77,12 +94,12 @@ public class Horrible_Evil_Password_Instument_Of_Doom_And_Despair{
 					});
 
 					buttonInst1.setBounds(0, 0, 80, 80);
-					WSPanel.add(buttonInst1).setMinimumSize(new Dimension(125, 100));
+					card1.add(buttonInst1).setMinimumSize(new Dimension(125, 100));
 					buttonInst1.setMaximumSize(new Dimension(125, 100));
 							buttonInst1.setPreferredSize(new Dimension(125, 100));
 
 						
-				JButton buttonInst2 = new JButton(Harmonica);
+				    buttonInst2 = new JButton(Harmonica);
 					buttonInst2.addActionListener(new ActionListener() {
 						public void actionPerformed(ActionEvent e)            {
 							//Here goes the action (method) you want to execute when clicked
@@ -93,11 +110,11 @@ public class Horrible_Evil_Password_Instument_Of_Doom_And_Despair{
     					}
 			
 					});
-					WSPanel.add(buttonInst2).setMinimumSize(new Dimension(125, 100));
+					card1.add(buttonInst2).setMinimumSize(new Dimension(125, 100));
 					buttonInst2.setMaximumSize(new Dimension(125, 100));
 					buttonInst2.setPreferredSize(new Dimension(125, 100));
 					
-				JButton buttonInst3 = new JButton(Guitar);
+				    buttonInst3 = new JButton(Guitar);
 
 					buttonInst3.addActionListener(new ActionListener() {
 						public void actionPerformed(ActionEvent e)            {
@@ -109,11 +126,11 @@ public class Horrible_Evil_Password_Instument_Of_Doom_And_Despair{
 								
 						});
 
-					WSPanel.add(buttonInst3).setMinimumSize(new Dimension(125, 100));
+					card1.add(buttonInst3).setMinimumSize(new Dimension(125, 100));
 					buttonInst3.setMaximumSize(new Dimension(125, 100));
 					buttonInst3.setPreferredSize(new Dimension(125, 100));
 						
-				JButton buttonInst4 = new JButton(CowBell);
+				    buttonInst4 = new JButton(CowBell);
 
 					buttonInst4.addActionListener(new ActionListener() {
 						public void actionPerformed(ActionEvent e)            {
@@ -124,67 +141,89 @@ public class Horrible_Evil_Password_Instument_Of_Doom_And_Despair{
 							}
 				
 					});
-					WSPanel.add(buttonInst4).setMinimumSize(new Dimension(125, 100));
+					card1.add(buttonInst4).setMinimumSize(new Dimension(125, 100));
 					buttonInst4.setMaximumSize(new Dimension(125, 100));
 					buttonInst4.setPreferredSize(new Dimension(125, 100));
 
 
 			
-				JButton button1 = new JButton("Next");
+				    button1 = new JButton("Next");
 
 					button1.addActionListener(new ActionListener() {
+                        @Override
 						public void actionPerformed(ActionEvent e)            {
 							System.out.println("hello");
-							PlayMusic(chosenInstrument);
-							if (chosenInstrument == "") {
-								JDialog noInstrument = new JDialog(f, ">:[");
+							if (chosenInstrument.isEmpty() || chosenInstrument == null) {
+								JDialog noInstrument = new JDialog(Horrible_Evil_Password_Instument_Of_Doom_And_Despair.this,">:[");
 								JLabel noInstrum = new JLabel("Please pick one!!!");
-									
+                                JDialog noInstrument1 = new JDialog(Horrible_Evil_Password_Instument_Of_Doom_And_Despair.this, ">:[");
+                                JLabel noInstrum1 = new JLabel("Please pick one!!!");
+                                JDialog noInstrument2 = new JDialog(Horrible_Evil_Password_Instument_Of_Doom_And_Despair.this, ">:[");
+                                JLabel noInstrum2 = new JLabel("Please pick one!!!");
 
-								noInstrument.add(noInstrum);
-								noInstrument.setSize(250, 100);
-								noInstrum.setFont(new Font("Serif", Font.BOLD, 20 ));
-								noInstrum.setHorizontalAlignment(JLabel.CENTER);
-								noInstrument.setVisible(true);
+									
+                                    noInstrument.add(noInstrum);
+                                    noInstrument.setSize(250, 100);
+                                    noInstrum.setFont(new Font("Serif", Font.BOLD, 20 ));
+                                    noInstrum.setHorizontalAlignment(JLabel.CENTER);
+                                    noInstrument.setVisible(true);
+                                    noInstrument.setLocation(((int)(Math.random() *800) + 50), ((int)(Math.random() *500) + 20));
+                                    
+                                    noInstrument1.add(noInstrum1);
+                                    noInstrument1.setSize(250, 100);
+                                    noInstrum1.setFont(new Font("Serif", Font.BOLD, 20 ));
+                                    noInstrum1.setHorizontalAlignment(JLabel.CENTER);
+                                    noInstrument1.setVisible(true);
+                                    noInstrument1.setLocation(((int)(Math.random() *1200) + 50), ((int)(Math.random() *700) + 20));
+
+                                     noInstrument2.add(noInstrum2);
+                                    noInstrument2.setSize(250, 100);
+                                    noInstrum2.setFont(new Font("Serif", Font.BOLD, 20 ));
+                                    noInstrum2.setHorizontalAlignment(JLabel.CENTER);
+                                    noInstrument2.setVisible(true);
+                                    noInstrument2.setLocation(((int)(Math.random() *800) + 50), ((int)(Math.random() *500) + 20));
 								//add error sound
-							}
+                                
+                            }
+
 							else {
-								//
-								
+								 instrument.show(mainPanel, "card2");
+        					
 							}
+				
 						}
 						
 					});
 					button1.setAlignmentX(FlowLayout.CENTER);
-				WSPanel.add(button1);
-
-			f.add(WSPanel);
-					
-
-					
+				
+        card1.add(button1);
+        
 		
-		f.setSize(400, 400);	
-		f.setVisible(true);
-	}
+		mainPanel.add(card1, "card1");
+        mainPanel.add(card2, "card2");
+			//card2.setPreferredSize(getSize());
+        
 
-	public static void PlayMusic(String location) {
-		try
-		{
-			File musicPath = new File(location);
-			if (musicPath.exists()) 
-			{
-				AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
-				Clip clip = AudioSystem.getClip();
-				clip.open(audioInput);
-				clip.start();
-			}
-			else
-			{
-				System.out.println("No file");
-			}
-		}
-		catch(Exception e) {
-				System.out.println(e);
-		}
-	}
-} 
+
+         buutun.addActionListener((e) -> {
+            instrument.show(mainPanel, "card1");
+        }
+    );
+
+		card2.add(buutun);
+
+
+        add(mainPanel);
+        setSize(500, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+
+
+
+    public static void main(String[] args) {
+        new Horrible_Evil_Password_Instument_Of_Doom_And_Despair();
+        
+    }
+}
